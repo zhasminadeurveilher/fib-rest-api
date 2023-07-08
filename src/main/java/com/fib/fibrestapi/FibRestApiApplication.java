4e28975aa9f384c73e9a46e583fa9f449f48cc12
Zhasmina Stoyanova <zhasmina.stoyanova@gmail.com>
2023-07-08 15:40:07 +0200

    #1 Create the boilerplate for the Spring boot project

---
+++ src/main/java/com/fib/fibrestapi/FibRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.fib.fibrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FibRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FibRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fib/fibrestapi/FibRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fib.fibrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FibRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
